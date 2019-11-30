5e3c41ee88b9d7409fa7dfea62a7927cf8dd7dd4
rizul <rizulgoyal23@gmail.com>
2019-11-30 13:54:04 -0500

    customer details page design done

--- app/src/main/java/com/example/c0766598_f2019_mad3125_midterm/Activities/CustomerDetailsActivity.java
+++ app/src/main/java/com/example/c0766598_f2019_mad3125_midterm/Activities/CustomerDetailsActivity.java
@@ -3,14 +3,23 @@ package com.example.c0766598_f2019_mad3125_midterm.Activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
+import com.example.c0766598_f2019_mad3125_midterm.ModelClasses.Customer;
 import com.example.c0766598_f2019_mad3125_midterm.R;
 
 public class CustomerDetailsActivity extends AppCompatActivity {
+
+    TextView custid, custname, custemail, custage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_details);
+
+        Customer custtemp =getIntent().getParcelableExtra("custobject");
+
+
+
     }
 }
