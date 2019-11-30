e158fbcbb4d3463e369317e66c6285175be9f248
rizul <rizulgoyal23@gmail.com>
2019-11-30 10:11:25 -0500

    splash screen done

--- app/src/main/java/com/example/c0766598_f2019_mad3125_midterm/MainActivity.java
+++ app/src/main/java/com/example/c0766598_f2019_mad3125_midterm/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.c0766598_f2019_mad3125_midterm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final int SPLASH_DISPLAY_LENGTH = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(MainActivity.this,LoginActivity.class);
+                MainActivity.this.startActivity(mainIntent);
+                MainActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
     }
-}
+
+
+    }
+
