a9624e4124ef0db9ba80e1d7ba2bdd8b266885f4
rizul <rizulgoyal23@gmail.com>
2019-11-30 11:17:43 -0500

    bill and customer class done

---
+++ app/src/main/java/com/example/c0766598_f2019_mad3125_midterm/ModelClasses/Bill.java
@@ -0,0 +1,113 @@
+package com.example.c0766598_f2019_mad3125_midterm.ModelClasses;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+import android.os.Parcelable.Creator;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Bill implements Parcelable
+{
+
+    @SerializedName("billId")
+    @Expose
+    private Integer billId;
+    @SerializedName("billDate")
+    @Expose
+    private String billDate;
+    @SerializedName("billType")
+    @Expose
+    private String billType;
+    @SerializedName("billAmount")
+    @Expose
+    private Double billAmount;
+    public final static Parcelable.Creator<Bill> CREATOR = new Creator<Bill>() {
+
+
+        @SuppressWarnings({
+                "unchecked"
+        })
+        public Bill createFromParcel(Parcel in) {
+            return new Bill(in);
+        }
+
+        public Bill[] newArray(int size) {
+            return (new Bill[size]);
+        }
+
+    }
+            ;
+
+    protected Bill(Parcel in) {
+        this.billId = ((Integer) in.readValue((Integer.class.getClassLoader())));
+        this.billDate = ((String) in.readValue((String.class.getClassLoader())));
+        this.billType = ((String) in.readValue((String.class.getClassLoader())));
+        this.billAmount = ((Double) in.readValue((Double.class.getClassLoader())));
+    }
+
+    /**
+     * No args constructor for use in serialization
+     *
+     */
+    public Bill() {
+    }
+
+    /**
+     *
+     * @param billAmount
+     * @param billId
+     * @param billType
+     * @param billDate
+     */
+    public Bill(Integer billId, String billDate, String billType, Double billAmount) {
+        super();
+        this.billId = billId;
+        this.billDate = billDate;
+        this.billType = billType;
+        this.billAmount = billAmount;
+    }
+
+    public Integer getBillId() {
+        return billId;
+    }
+
+    public void setBillId(Integer billId) {
+        this.billId = billId;
+    }
+
+    public String getBillDate() {
+        return billDate;
+    }
+
+    public void setBillDate(String billDate) {
+        this.billDate = billDate;
+    }
+
+    public String getBillType() {
+        return billType;
+    }
+
+    public void setBillType(String billType) {
+        this.billType = billType;
+    }
+
+    public Double getBillAmount() {
+        return billAmount;
+    }
+
+    public void setBillAmount(Double billAmount) {
+        this.billAmount = billAmount;
+    }
+
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeValue(billId);
+        dest.writeValue(billDate);
+        dest.writeValue(billType);
+        dest.writeValue(billAmount);
+    }
+
+    public int describeContents() {
+        return 0;
+    }
+
+}
--- app/src/main/java/com/example/c0766598_f2019_mad3125_midterm/ModelClasses/Customer.java
+++ app/src/main/java/com/example/c0766598_f2019_mad3125_midterm/ModelClasses/Customer.java
@@ -1,4 +1,144 @@
 package com.example.c0766598_f2019_mad3125_midterm.ModelClasses;
 
-public class Customer {
+import android.os.Parcel;
+import android.os.Parcelable;
+import android.os.Parcelable.Creator;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.ArrayList;
+
+public class Customer implements Parcelable
+{
+
+    @SerializedName("id")
+    @Expose
+    private Integer id;
+    @SerializedName("firstName")
+    @Expose
+    private String firstName;
+    @SerializedName("lastName")
+    @Expose
+    private String lastName;
+    @SerializedName("age")
+    @Expose
+    private Integer age;
+    @SerializedName("email")
+    @Expose
+    private String email;
+    @SerializedName("bill")
+    @Expose
+    private ArrayList<Bill> billarray;
+    public final static Parcelable.Creator<Customer> CREATOR = new Creator<Customer>() {
+
+
+        @SuppressWarnings({
+                "unchecked"
+        })
+        public Customer createFromParcel(Parcel in) {
+            return new Customer(in);
+        }
+
+        public Customer[] newArray(int size) {
+            return (new Customer[size]);
+        }
+
+    }
+            ;
+
+    protected Customer(Parcel in) {
+        this.id = ((Integer) in.readValue((Integer.class.getClassLoader())));
+        this.firstName = ((String) in.readValue((String.class.getClassLoader())));
+        this.lastName = ((String) in.readValue((String.class.getClassLoader())));
+        this.age = ((Integer) in.readValue((Integer.class.getClassLoader())));
+        this.email = ((String) in.readValue((String.class.getClassLoader())));
+        this.billarray = ((ArrayList<Bill>) in.readValue((Bill.class.getClassLoader())));
+    }
+
+    /**
+     * No args constructor for use in serialization
+     *
+     */
+    public Customer() {
+    }
+
+    /**
+     *
+     * @param firstName
+     * @param lastName
+     * @param id
+     * @param age
+     * @param email
+     */
+    public Customer(Integer id, String firstName, String lastName, Integer age, String email) {
+        super();
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.email = email;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public ArrayList<Bill> getBillarray() {
+        return billarray;
+    }
+
+    public void setBill(Bill bill) {
+        billarray.add(bill);
+
+    }
+
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeValue(id);
+        dest.writeValue(firstName);
+        dest.writeValue(lastName);
+        dest.writeValue(age);
+        dest.writeValue(email);
+        dest.writeValue(billarray);
+    }
+
+    public int describeContents() {
+        return 0;
+    }
+
 }
